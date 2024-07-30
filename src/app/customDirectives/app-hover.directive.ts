b4ad87426ee2f084609293664e687695b53a04c5
Munawar Ali M <munawaralimangat@gmail.com>
2024-07-30 15:32:33 +0530

    property binding in directives

--- src/app/customDirectives/app-hover.directive.ts
+++ src/app/customDirectives/app-hover.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appAppHover]'
+  selector: '[appHover]'
 })
 export class AppHoverDirective {
 
-  constructor() { }
+  constructor(
+    private element:ElementRef,
+    private renderer:Renderer2
+  ) { }
 
+  @HostBinding('style.backgroundColor')
+  backgroundColor:string = 'darkRed'
+
+  @HostBinding('style.color')
+  color:string = 'white'
+
+  //hostBinding can set/bind some properties in to the element 
+
+  //hostListener can set or unset some properties on events, such as mouse hover
+  @HostListener('mouseenter') mouseEnter(){
+    this.backgroundColor = 'skyBlue'
+    this.color = 'black'
+  }
+  
+  @HostListener('mouseout') mouseOut(){
+    this.backgroundColor = 'darkRed'
+    this.color = 'white'
+  }
 }
--- src/app/customDirectives/setBackground.directive.ts
+++ src/app/customDirectives/setBackground.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, OnInit, Renderer2, HostListener } from "@angular/core";
 
 @Directive({
     selector:'[setBackground]'
 })
 
 export class setBackground implements OnInit{
-    
     constructor(private element:ElementRef,private renderer:Renderer2){
     }
+
+    @Input('setBackground') backColor:string = 'black';
+    @Input('setText') textColor:string = 'white';
+    @Input() title:string = 'Hello world'
 
     ngOnInit(){
         // this.element.nativeElement.style.backgroundColor = '#C0C0C0'
         // this.element.nativeElement.style.color = 'black'
-        this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#C0C0C0')
-        this.renderer.setStyle(this.element.nativeElement,'color','black')
+        this.renderer.setStyle(this.element.nativeElement,'backgroundColor',this.backColor)
+        this.renderer.setStyle(this.element.nativeElement,'color',this.textColor)
     }
 }
