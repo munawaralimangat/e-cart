d7d7c2c929dbe9ea394bd22be86d44819f36ca48
Munawar Ali M <munawaralimangat@gmail.com>
2024-07-31 11:40:45 +0530

    upds

--- src/app/customDirectives/disable-product.directive.ts
+++ src/app/customDirectives/disable-product.directive.ts
@@ -6,9 +6,7 @@ import { Directive, ElementRef, Renderer2, Input } from "@angular/core";
 
 
 export class DisableProductDirective {
-    constructor(private element:ElementRef,private renderer:Renderer2){
-
-    }
+    constructor(private element:ElementRef,private renderer:Renderer2){}
 
     @Input() set disableProduct(disable:boolean){
         if(!disable){
