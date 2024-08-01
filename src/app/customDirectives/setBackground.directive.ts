64beb974e62cc62ac69218d5bbd1af3e55703059
Munawar Ali M <munawaralimangat@gmail.com>
2024-08-01 12:41:56 +0530

    updts

--- src/app/customDirectives/setBackground.directive.ts
+++ src/app/customDirectives/setBackground.directive.ts
@@ -11,11 +11,11 @@ export class setBackground implements OnInit{
     // @Input('setBackground') backColor:string = 'black';
     // @Input('setText') textColor:string = 'white';
 
-    @Input('setBackground') changeTextandBackColor: {backColor:string,textColor:string}
+    @Input('setBackground') changeTextandBackColor: { backColor:string, textColor:string }
 
     ngOnInit(){
-        // this.element.nativeElement.style.backgroundColor = '#C0C0C0'
-        // this.element.nativeElement.style.color = 'black'
+        // this.element.nativeElement.style.backgroundColor = '#C0C0C0';
+        // this.element.nativeElement.style.color = 'black';
         this.renderer.setStyle(this.element.nativeElement,'backgroundColor',this.changeTextandBackColor.backColor)
         this.renderer.setStyle(this.element.nativeElement,'color',this.changeTextandBackColor.textColor)
     }
